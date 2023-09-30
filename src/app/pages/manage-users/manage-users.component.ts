669c17697546ff8a661166f2d943cc235e01eb23
pavan <pavan@wibesapp.com>
2023-09-30 15:02:10 +0530

    users model added

--- src/app/pages/manage-users/manage-users.component.ts
+++ src/app/pages/manage-users/manage-users.component.ts
@@ -7,4 +7,43 @@ import { Component } from '@angular/core';
 })
 export class ManageUsersComponent {
 
+
+  public users:any = [
+    {
+      id:"DF3245FG",
+      role:"user",
+      f_name:"Mahesh",
+      l_name:"Raju",
+      email:"maheshraju@wibesapp.com",
+      mobile:"+917382037337",
+      avatar_url:"",
+      wishlist:[],
+      orders:[],
+      support:[],
+      addresses:[
+        {
+          name:"Home",
+          h_no:"Plot 123",
+          add_line1:"ABC Apartments, road/lane",
+          add_line2:"Locality/colony, City/town/village",
+          state:"State1",
+          country:"",
+          pincode:"123456"
+        }
+      ],
+
+    }
+  ]
+
+
+  constructor()
+  {
+
+  }
+
+  ngOnInit()
+  {
+
+  }
+
 }
